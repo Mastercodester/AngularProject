9f2fecd5d9efcceaa0a8f34006784babb331b5d5
sean carter <ajacks75@emich.edu>
2025-07-10 19:37:29 -0400

    July 10

--- AngularProject/src/main.ts
+++ AngularProject/src/main.ts
@@ -17,4 +17,13 @@ bootstrapApplication(App, appConfig)
   //8.create clocfks component
   //9.create ClockForm
   //10. create clockDisplay
-  //11.Connect components with service
+  //11.Connect components with service  JUly9
+////////////////////////////////////////////////////////////////
+
+
+
+//1.add other  options on settings.html
+//2.connect settings html with clock html 
+//3. connect settings css with clock component css
+//4. add clock form to setting component
+//5. add clock display to clock component
